899097d69d709a491202a2f3f4c7fa3f2c86c680
Lydia Portillo <lydiaportillo@Lydias-MacBook-Pro.local>
2016-12-13 14:08:52 -0800

    add everything

---
+++ js/aboutSlide.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function () {
+
+    $('.CheetahCheetah').tooltip();
+
+    $('.LionLion').tooltip();
+
+    $('.LeopardLeopard').tooltip();
+
+    $('.WildeWilde').tooltip();
+
+    $('.ZebraZebra').tooltip();
+
+    $('.ZebraZebra').tooltip();
+
+    $('.Elphante').tooltip();
+
+    $('.btn').click(function() {
+        alert("Text: " + $("#test").text());
+
+});
+    });
+
+
+
